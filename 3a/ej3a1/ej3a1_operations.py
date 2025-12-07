9e7e622d430d906c498d9b446b9dd72893dbd7fb
jdelegon-wq <jdelegon@uoc.edu>
2025-12-07 11:38:06 +0100

    Implement basic arithmetic operations with docstrings

--- 3a/ej3a1/ej3a1_operations.py
+++ 3a/ej3a1/ej3a1_operations.py
@@ -3,19 +3,26 @@
 
 def add(num_1: float, num_2: float) -> float:
     # Write here your code
-    pass
+    """Devuelve la suma de dos números"""
+    return num_1 + num_2
 
 
 def subtract(num_1: float, num_2: float) -> float:
     # Write here your code
-    pass
+    """Devuelve la resta de dos números"""
+    return num_1 - num_2
 
 
 def multiply(num_1: float, num_2: float) -> float:
     # Write here your code
-    pass
+    """Devuelve el producto de dos números"""
+    return num_1 * num_2
 
 
 def divide(num_1: float, num_2: float) -> float:
     # Write here your code
-    pass
+    """Devuelve la división de dos números"""
+    """Maneja división entre 0"""
+    if num_2 == 0:
+        raise ValueError("No se puede dividir entre 0"""
+    return num_1 / num_2
