0cfe4f58193d44e75d4007bd4fe0c37b0e5fe8ba
jdelegon-wq <jdelegon@uoc.edu>
2025-12-07 12:00:16 +0100

    Add imports for string to datetime conversion

--- 3a/ej3a3/ej3a3_time_package/__init__.py
+++ 3a/ej3a3/ej3a3_time_package/__init__.py
@@ -1,4 +1,5 @@
 # write import for convert_date_format
-from .convert_date_format import datetime
+from .convert_date_format import string_to_datetime,
+datetime_to_string
 # write import for date_operations
 from .dateoperations import datetime
