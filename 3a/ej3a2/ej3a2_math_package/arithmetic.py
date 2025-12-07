6ed4255eebfb03b0963ae6cbab3a527905010a39
jdelegon-wq <jdelegon@uoc.edu>
2025-12-07 11:45:42 +0100

    Implement power and square root functions

--- 3a/ej3a2/ej3a2_math_package/arithmetic.py
+++ 3a/ej3a2/ej3a2_math_package/arithmetic.py
@@ -3,10 +3,13 @@ import math
 
 def power(base: float, exponent: float) -> float:
 	# Write your code here
-	pass
+	"""Devuelve el resultado de elevar un número a una poténcia"""
+	return base ** exponent
 
 def square_root(num_1: float) -> float:
 	# Write your code here
-	pass
+	"""Devuelve la raiz cuadrada de un número"""
+	return math.sqrt(num_1)
+	"""O num_1 ** 0.5"""
 
 
